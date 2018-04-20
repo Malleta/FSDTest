dbb53247603bbe82d8153979bae8ded5d34ee93f
Nikola <nmmaleta@gmail.com>
2018-04-20 15:30:06 +0200

    2.0

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!doctype <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,54 +9,52 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
     <?php wp_head(); ?>
 </head>
+
 <body>
-    
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <div class="container">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
-      <a class="navbar-brand" href="#"><b style="font-size: 23px">FSD</b> <label style="font-size: 15px">TEST</label></a>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent"> 
-      <ul class="navbar-nav">
-          <li class="nav-item">
-            <button class="btn btn-sm btn-home">HOME</button>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">ABOUT</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">SERVICES</a>
-          </li>
-          <li class="nav-item">
-          <div class="dropdown">
-            <button class="btn btn-sm btn-dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <!-- Pokazivac napraviti -->
-            <i class="fas fa-angle-down"></i>    
-                PORTFOLIO
+
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Action</a>
-                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Another action</a>
-                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Something else here</a>
+            <a class="navbar-brand" href="#"><b style="font-size: 23px">FSD</b> <label style="font-size: 15px">TEST</label></a>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <button class="btn btn-sm btn-home">HOME</button>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">ABOUT</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">SERVICES</a>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <button class="btn btn-sm btn-dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <!-- Pokazivac napraviti -->
+                                <i class="fas fa-angle-down"></i> PORTFOLIO
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Action</a>
+                                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Another action</a>
+                                <a class="dropdown-item" href="#"><i class="fas fa-angle-right"></i>Something else here</a>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">BLOG</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FEATURES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CONTANTS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-search"></i></a>
+                    </li>
+                </ul>
             </div>
-            </div>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">BLOG</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">FEATURES</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">CONTANTS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#"><i class="fas fa-search"></i></a>
-          </li>
-        </ul>
         </div>
-      </div>
     </nav>
-
-
--- index.php
+++ index.php
@@ -1,65 +1,79 @@
 <?php get_header(); ?>
  
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="<?php echo get_template_directory_uri()?>/css/img/slide-img.png" alt="First slide">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="<?php echo get_template_directory_uri()?>/css/img/slide-img.png" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<?php echo get_template_directory_uri()?>/css/img/slide-img.png" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="<?php echo get_template_directory_uri()?>/css/img/slide-img.png" alt="Third slide">
+        </div>
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="<?php echo get_template_directory_uri()?>/css/img/slide-img.png" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="<?php echo get_template_directory_uri()?>/css/img/slide-img.png" alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
- 
- <div class="container">
-     <div class="row justify-content-center">
-         <div class="col-md tab mr-4">
-             <div class="tab-icon"><i class="far fa-thumbs-up"></i></div>
-             <div class="tab-title"><h5>SUSPENDISSE</h5></div>
-             <div class="tab-details"><p>gibberish gibb erish gibb erishgibb erishgibberish gibbe rish gib beri sh</p></div>
-             <div class="tab-btn"><button class="btn">Button</button></div>
-         </div>
-         <div class="col-md tab mr-4">
-             <div class="tab-icon"><i class="fas fa-key"></i></div>
-             <div class="tab-title"><h5>MAECENAS</h5></div>
-             <div class="tab-details"><p>gibberishgibb erish gi bberishgibb eris hg ibberish giib beri sh</p></div>
-             <div class="tab-btn"><button class="btn">Button</button></div>
-         </div>
-         <div class="col-md tab mr-4">
-             <div class="tab-icon"><i class="fas fa-flag"></i></div>
-             <div class="tab-title"><h5>ALIQUAM</h5></div>
-             <div class="tab-details"><p>gibberish gibberish gibb erishgibb erishgibberish gibbe riberish</p></div>
-             <div class="tab-btn"><button class="btn">Button</button></div>
-         </div>
-         <div class="col-md tab mr-4">
-             <div class="tab-icon"><i class="fas fa-flask"></i></div>
-             <div class="tab-title"><h5>HABITASSE</h5></div>
-             <div class="tab-details"><p>gibberish gibb erishgibb erishgibb eris hgibberish gibbeis</p></div>
-             <div class="tab-btn"><button class="btn">Button</button></div>
-         </div>
-     </div>
- </div>
 
-
-
-
-<!-- aa -->
-
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md tab mr-4">
+            <div class="tab-icon"><i class="far fa-thumbs-up"></i></div>
+            <div class="tab-title">
+                <h5>SUSPENDISSE</h5></div>
+            <div class="tab-details">
+                <p>gibberish gibb erish gibb erishgibb erishgibberish gibbe rish gib beri sh</p>
+            </div>
+            <div class="tab-btn">
+                <button class="btn">Button</button>
+            </div>
+        </div>
+        <div class="col-md tab mr-4">
+            <div class="tab-icon"><i class="fas fa-key"></i></div>
+            <div class="tab-title">
+                <h5>MAECENAS</h5></div>
+            <div class="tab-details">
+                <p>gibberishgibb erish gi bberishgibb eris hg ibberish giib beri sh</p>
+            </div>
+            <div class="tab-btn">
+                <button class="btn">Button</button>
+            </div>
+        </div>
+        <div class="col-md tab mr-4">
+            <div class="tab-icon"><i class="fas fa-flag"></i></div>
+            <div class="tab-title">
+                <h5>ALIQUAM</h5></div>
+            <div class="tab-details">
+                <p>gibberish gibberish gibb erishgibb erishgibberish gibbe riberish</p>
+            </div>
+            <div class="tab-btn">
+                <button class="btn">Button</button>
+            </div>
+        </div>
+        <div class="col-md tab mr-4">
+            <div class="tab-icon"><i class="fas fa-flask"></i></div>
+            <div class="tab-title">
+                <h5>HABITASSE</h5></div>
+            <div class="tab-details">
+                <p>gibberish gibb erishgibb erishgibb eris hgibberish gibbeis</p>
+            </div>
+            <div class="tab-btn">
+                <button class="btn">Button</button>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php get_footer(); ?>
